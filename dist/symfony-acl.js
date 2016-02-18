61c263387c74009183881f58ffd4bc45db5d82bb
Rafał Lorenz <vardius@gmail.com>
2016-02-18 09:02:47 +0100

    UMD wrap module

--- dist/symfony-acl.js
+++ dist/symfony-acl.js
@@ -6,11 +6,24 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-(function () {
+(function (root, factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['angular'], factory);
+    } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
+        module.exports = factory(require('angular'));
+    } else {
+        return factory(root.angular);
+    }
+}(this, symfonyAcl));
+
+function symfonyAcl(angular) {
     'use strict';
 
+    var moduleName = 'symfony-acl';
+
     angular
-        .module('symfony-acl', [])
+        .module(moduleName, [])
         .run(['$rootScope', 'AccessService', run])
         .provider('AccessService', AccessService)
     ;
@@ -169,4 +182,5 @@
         };
     }
 
-    )();
+    return moduleName;
+}
